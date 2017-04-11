cd3862b351df3f724312899e9639e5e5f018a6aa
Codetector <codetector@codetector.cn>
2017-04-10 21:00:01 -0400

    options exception

--- src/main/kotlin/cn/com/guardiantech/checkin/server/config/WebSecurityConfig.kt
+++ src/main/kotlin/cn/com/guardiantech/checkin/server/config/WebSecurityConfig.kt
@@ -4,6 +4,7 @@ import cn.com.guardiantech.checkin.server.authentication.AuthenticationFilter
 import cn.com.guardiantech.checkin.server.service.AuthenticationService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -20,12 +21,14 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter(){
     lateinit var authenticationService: AuthenticationService
 
     override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**")
         web.ignoring()
                 .antMatchers("/auth/auth")
                 .antMatchers("/auth/login")
                 .antMatchers("/auth/register")
                 //Event
                 .antMatchers("/event/list")
+                //Signup
     }
 
     override fun configure(http: HttpSecurity) {
