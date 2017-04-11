ce069ee238b71a1a2aa2c075851b29754c4aef42
Codetector <codetector@codetector.cn>
2017-04-11 19:55:52 -0400

    Http NotAcceptable

--- src/main/kotlin/cn/com/guardiantech/checkin/server/controller/ControllerExceptionHandler.kt
+++ src/main/kotlin/cn/com/guardiantech/checkin/server/controller/ControllerExceptionHandler.kt
@@ -4,6 +4,7 @@ import cn.codetector.jet.jetsimplejson.JSONObject
 import cn.com.guardiantech.checkin.server.exception.UnauthorizedException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
@@ -20,4 +21,7 @@ class ControllerExceptionHandler {
 
     @ExceptionHandler(UnauthorizedException::class)
     fun unAuthorizedExceptionHandler() = ResponseEntity(JSONObject().put("error", "unauthorized").encode(), HttpStatus.UNAUTHORIZED)
+
+    @ExceptionHandler(MissingServletRequestParameterException::class)
+    fun missingParamException(e: MissingServletRequestParameterException) = ResponseEntity(JSONObject().put("error", "missingParam").put("name", e.parameterName).put("type", e.parameterType).encode(), HttpStatus.NOT_ACCEPTABLE)
 }
