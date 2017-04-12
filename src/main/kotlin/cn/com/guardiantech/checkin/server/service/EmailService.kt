cf415a05aa294ac380b6b8ddf72bd6c085f13c5a
Codetector <codetector@codetector.cn>
2017-04-12 14:11:39 -0400

    try again

--- src/main/kotlin/cn/com/guardiantech/checkin/server/service/EmailService.kt
+++ src/main/kotlin/cn/com/guardiantech/checkin/server/service/EmailService.kt
@@ -1,6 +1,7 @@
 package cn.com.guardiantech.checkin.server.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 import javax.mail.Address
@@ -13,11 +14,15 @@ import javax.mail.internet.InternetAddress
  */
 @Service
 class EmailService {
+    @Value("\${spring.mail.from}")
+    lateinit var from: String
+
     @Autowired
     lateinit var mailSender: JavaMailSender
 
     fun sendMail(templateName: String, values: HashMap<String, String>, vararg recipients: String) {
         val msg = mailSender.createMimeMessage()
+        msg.setFrom(from)
         msg.subject = "AOF Check In Test Mail"
         msg.setText("This is sample text")
         recipients.mapTo(HashSet<Address>(), ::InternetAddress).forEach {
